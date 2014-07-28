6c5248b978259655467969b407a3d4f50f0d0bf1
Adam Ginsburg <keflavich@gmail.com>
2014-07-28 12:27:23 -0600

    added mkfifo copied from cleese

---
+++ .mkfifo.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+FIFO=~/.fifo_vars
+[ -e $FIFO ] && cat $FIFO > /dev/null || mkfifo $FIFO
+
+# save number of variables that follow
+NVARS=1
+echo $NVARS > $FIFO
+echo DISPLAY=$DISPLAY > $FIFO
+#echo ENV2=sth2 > $FIFO
+#Start that script in the background on login (it will only terminate when all variables are read from it).
+
+#Now you can read from the fifo, e.g. add this function to your .bashrc
+
+#so that you can in your old screen session
+#update_session
